8b603115c1dfc4e2879f972807aa256103ecd233
geekwims <suyoung154@gmail.com>
2017-05-10 11:44:12 +0900

    IE8 Promise 호환성 추가

--- src/index.js
+++ src/index.js
@@ -4,6 +4,13 @@ import App from './App';
 import 'semantic-ui-css/semantic.min.css';
 import './index.css';
 import './Animation.css';
+import Promise from 'promise-polyfill';
+
+
+// for IE 8
+if (!window.Promise) {
+  window.Promise = Promise;
+}
 
 ReactDOM.render(
   <App />,
